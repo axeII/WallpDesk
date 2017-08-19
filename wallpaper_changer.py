7f20af19386cfb7e036093397793e8b65d7c0acd
Ales Lerch <ales.lerch@gmail.com>
2017-08-19 15:46:49 +0200

    wallaper changer setup

--- wallpaper_changer.py
+++ wallpaper_changer.py
@@ -8,4 +8,27 @@ __author__ = 'ales lerch'
 class Wall_changer:
 
     def __init__(self):
+        """connect this module to database one"""
         pass
+
+    def set_wallpaper(self,img):
+        db_file = "~/Library/Application Support/Dock/desktoppicture.db"
+        subprocess.call(["sqlite3", db_file, f"update data set value = '{img}'"])
+        subprocess.call(["killall", "Dock"])
+
+    def set_timer(self):
+        pass
+
+    def choose_image(self):
+        """choose dark or light based on time"""
+        pass
+
+    def choose_random_image(self):
+        """choose dark or light based on time but random"""
+        pass
+
+    def set_directory(self):
+        """set directory for choosing images to set as wallaper desktop"""
+        pass
+
+
