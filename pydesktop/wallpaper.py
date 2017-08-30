9868ead02268fb502baeec4369abe18234125c76
Ales Lerch <ales.lerch@gmail.com>
2017-08-30 20:02:27 +0200

    wallpaper edit, queue added, load img database fixed

--- pydesktop/wallpaper.py
+++ pydesktop/wallpaper.py
@@ -12,28 +12,37 @@ import analyzer
 import database
 import datetime
 import threading
+from queue import Queue
 
 class Editor(wall.Paper):
 
     def __init__(self,dir_with_imgs):
         """modules, load image to database by name,path,type as dict/obj"""
         self.set_timer()
-        self.directory = dir_with_imgs
         self.db = database.DB_lite("./")
+        self.directory = dir_with_imgs
+        self.que = Queue(maxsize = 1)
+        self.que.put(threading.Thread(target=self.loop))
         super().__init__(dir_with_imgs)
 
         self.load_thread = threading.Thread(target=self.load_img_database)
-        #self.load_thread.start()
+        self.load_thread.start()
 
     def load_img_database(self):
+        """ warning loading database can now only work 
+        with thread safe database no ther thread can work with database"""
+        print("loading images...")
         super().get_images_files()
-        for img in self.img_files:
-            data = {
-                "name" : os.path.basename(img),
-                "path" : os.path.abspath(dir_with_imgs),
-                "type" : analyzer.check_image_color(f"{os.path.abspath(dir_with_imgs)}/{img}"),
-            }
-            self.db.new_item(data)
+        if sorted(self.img_files) != sorted(self.db.get_names()):
+            for img in self.img_files:
+                data = {
+                    "name" : os.path.basename(img),
+                    "path" : os.path.abspath(self.directory),
+                    "type" : analyzer.check_image_color(
+                        f"{os.path.abspath(self.directory)}/{img}"),
+                }
+                self.db.new_item(data)
+            print("all images loaded to databse")
 
     def set_timer(self, seconds = 3600):
         """ Based on time wallpaper can change"""
@@ -56,9 +65,9 @@ class Editor(wall.Paper):
             theme = "light"
 
         if not self.load_thread.isAlive():
-            images = self.db.get_items( type_ = theme)
+            images = self.db.get_items(type_ = theme)
             image = images[random.choice(list(images.keys()))]
-            super().set_wallpaper(f"{image[1]}/{image[2]}")
+            super().set_wallpaper(f"{image[1]}/{image[0]}")
 
     def run(self):
         if self.que.empty():
@@ -67,8 +76,7 @@ class Editor(wall.Paper):
 
     def loop(self):
         while True:
-            time.sleep(10.0)
-
+            time.sleep(self.time)
             if self.interrupted:
                 break
             self.choose_random_image()
