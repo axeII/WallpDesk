2918e54e4e36f0e22d0716f41a5039d94840cc46
Ales Lerch <ales.lerch@gmail.com>
2017-08-30 20:48:33 +0200

    seting main

---
+++ pydesktop/__main__.py
@@ -0,0 +1,15 @@
+"""WallpDesk
+
+Simple os x application for setting wallpaper and
+desktop file name commands
+"""
+__author__ = 'ales lerch'
+
+from menu import Bar
+
+def main():
+    walpdesk = Bar()
+    walpdesk.run()
+
+if __name__ == "__main__":
+    main()
