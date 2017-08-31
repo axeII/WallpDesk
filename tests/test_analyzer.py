015c371130c9c43ca26a59dabc82f3b193740905
Ales Lerch <ales.lerch@gmail.com>
2017-08-31 15:43:36 +0200

    edit tests/test_analyzer

--- tests/test_analyzer.py
+++ tests/test_analyzer.py
@@ -9,8 +9,14 @@ class TestAnalyzer(unittest.TestCase):
         pass
 
     def test_check_image_color(self):
+        """> Check image for color(light)"""
         result = analyzer.check_image_color("tests/test_files/sample.jpg")
         self.assertEqual(result, "light")
+
+    def test_check_wrong_image(self):
+        """> Check non exists image(Not found)"""
+        result = analyzer.check_image_color("tests/test_files/non_exists.jpg")
+        self.assertEqual(result, "Image not found")
 
 if __name__ == "__main__":
     unittest.main()
